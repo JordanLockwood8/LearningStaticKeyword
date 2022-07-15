916c695094f574713515a9ea212e0bdd4b7da7db
Jordan Lockwood <8xavienlockwood8@gmail.com>
2022-07-15 13:23:51 -0600

    first commit E1

---
+++ StaticExcersize/Program.cs
@@ -0,0 +1,5 @@
+﻿using StaticExcersize;
+Console.WriteLine("Celcius:" + TempConverter.FahrenheitToCelsius(32));
+Console.WriteLine("Celcius:" + TempConverter.FahrenheitToCelsius(100));
+Console.WriteLine("Fahrenheit:" + TempConverter.CelsiusToFahrenheit(0));
+Console.WriteLine("Fahrenheit:" + TempConverter.CelsiusToFahrenheit(37.00));
---
+++ StaticExcersize/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExcersize
+{
+    internal static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahr)
+        {
+
+            double cel = (fahr - 32) * 5 / 9;
+            return cel;
+            
+        }
+        public static double CelsiusToFahrenheit(double cel)
+        {
+
+            double fahr = (cel * 9 / 5) + 32;
+            return fahr;
+        }
+    }
+}
